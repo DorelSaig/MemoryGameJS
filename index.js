9d46b0d90e8b1f8ac7571e665932896342bdc19c
MayaGembom <mayagembom3007@gmail.com>
2021-12-02 13:37:27 +0200

    we can choose 4*4 or 5*5 board!

--- index.js
+++ index.js
@@ -1,150 +1,63 @@
-var icons_array = [
-  "/assets/STK-20211117-WA0011.webp",
-  "/assets/STK-20211117-WA0011.webp",
-  "/assets/STK-20210823-WA0078.webp",
-  "/assets/STK-20210823-WA0078.webp",
-  "/assets/STK-20210822-WA0075.webp",
-  "/assets/STK-20210822-WA0075.webp",
-  "/assets/STK-20211027-WA0033.webp",
-  "/assets/STK-20211027-WA0033.webp",
-  "/assets/STK-20210823-WA0069.webp",
-  "/assets/STK-20210823-WA0069.webp",
-  "/assets/STK-20211109-WA0005.webp",
-  "/assets/STK-20211109-WA0005.webp",
-  "/assets/STK-20210811-WA0037.webp",
-  "/assets/STK-20210811-WA0037.webp",
-  "/assets/STK-20211007-WA0015.webp",
-  "/assets/STK-20211007-WA0015.webp",
-  "/assets/STK-20211122-WA0017.webp",
-  "/assets/STK-20211122-WA0017.webp",
-  "/assets/STK-20211009-WA0047.webp",
-  "/assets/STK-20211009-WA0047.webp",
-  "/assets/STK-20211110-WA0002.webp",
-  "/assets/STK-20211110-WA0002.webp",
-  "/assets/STK-20211105-WA0012.webp",
-  "/assets/STK-20211105-WA0012.webp",
-  "/assets/STK-20211126-WA0017.webp",
-];
-var vals = [];
-var cards_ids = [];
-var cards_flipped = 0;
-var delay = 1200;
-var size = 0;
+// scritps.js
+const cards = document.querySelectorAll('.memory-card');
 
-Array.prototype.memory_tile_shuffle = function () {
-  var i = size,
-    j,
-    temp;
-  while (--i > 0) {
-    j = Math.floor(Math.random() * (i + 1));
-    temp = this[j];
-    this[j] = this[i];
-    this[i] = temp;
+var hasFlippedCard = false;
+var lockBoard = false;
+var firstCard, secondCard;
+
+
+function flipCard() {
+  if (lockBoard) 
+    return;
+  if (this === firstCard) 
+    return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) { // first click
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
   }
-};
-
-//newBoard();
-get();
-
-function get() {
-  var startString = "";
-  console.log(size);
-  startString +=
-    '<center><input type="button" value="Start Game" onclick="newBoard()" /></center>';
-  document.getElementById("memory_board").innerHTML = startString;
+  secondCard = this;
+  checkForMatch();
 }
 
-function newBoard() {
-  //Board Size Setting.
-  var theBoard = document.getElementById("memory_board");
+function checkForMatch() {
+  var isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatch ? disableCards() : unflipCards();
 
-  console.log(size);
-
-  if (size == 16) {
-    theBoard.style.height = "700px";
-    theBoard.style.width = "700px";
-  } else {
-    theBoard.style.height = "880px";
-    theBoard.style.width = "880px";
-  }
-
-  cards_flipped = 0;
-  var output = "";
-  icons_array.memory_tile_shuffle();
-  for (let i = 0; i < size; i++) {
-    output +=
-      '<div id="tile_' +
-      i +
-      '" class="flip-card" onclick=" memoryFlipTile(this,\'' +
-      icons_array[i] +
-      "');\">" +
-      '<div class = "flip-card-inner">' +
-      '<div class = "flip-card-back">' +
-      "<img src = " +
-      icons_array[i] +
-      ' alt = "Avatar"' +
-      ' style = "width:110px;height:110px;">' +
-      "</div>" +
-      '<div class = "flip-card-front" >' +
-      "</div>" +
-      "</div>" +
-      "</div>";
-  }
-  document.getElementById("memory_board").innerHTML = output;
 }
 
-function flipCard(tile) {
-  tile.classList.toggle("flipCard");
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  
+  resetBoard();
 }
 
-function memoryFlipTile(tile, val) {
-  flipCard(tile);
-  if (vals.length < 2) {
-    if (vals.length == 0) {
-      //No Card Was Flipped Yet
-      vals.push(val);
-      cards_ids.push(tile.id);
-    } else if (vals.length == 1) {
-      vals.push(val);
-      cards_ids.push(tile.id);
-      if (vals[0] == vals[1]) {
-        cards_flipped += 2; //Cards flipped counter
+function unflipCards() {
+  lockBoard = true;
 
-        //Disable the click function for the matched cards
-        var firstGuess = document.getElementById(cards_ids[0]);
-        var secondGuess = document.getElementById(cards_ids[1]);
-        firstGuess.onclick = null;
-        secondGuess.onclick = null;
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
 
-        // Clear both arrays
-        vals = [];
-        cards_ids = [];
-
-        // Check to see if the whole board is cleared
-        if (cards_flipped == size) {
-          alert("Board cleared... generating new board");
-          document.getElementById("memory_board").innerHTML = "";
-          newBoard();
-        }
-      } else {
-        function flip2Back() {
-          // Flip the 2 tiles back over
-          var tile_1 = document.getElementById(cards_ids[0]);
-          var tile_2 = document.getElementById(cards_ids[1]);
-
-          // tile_1.style.backgroundColor = "darkblue";
-          // tile_2.style.background = "darkblue";
-          //tile_2.innerHTML = "";
-          // Clear both arrays
-          vals = [];
-          cards_ids = [];
-          flipCard(tile_1);
-          flipCard(tile_2);
-          // tile_1.classList.remove("flip-card-front");
-          // tile_2.classList.remove("flip-card-front");
-        }
-        setTimeout(flip2Back, delay);
-      }
-    }
-  }
+    resetBoard();
+  }, 1000);
 }
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    var randomPos = Math.floor(Math.random() * 16);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
